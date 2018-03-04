bec53789c35173facbc8657ef449b5e0524d73e6
Oseias Beu <oseiasbeu@outlook.com>
2018-03-03 22:34:57 -0300

    Página inicial, falta nav-bar e footer

---
+++ teste/script.js
@@ -0,0 +1,7 @@
+let boto = document.querySelector('.btn');
+
+function efeito(){
+  boto.style.backgroundColor = 'red';
+  boto.style.transform = 'scale(2.5,2.5)';
+  boto.style.transition= '1s';
+}
